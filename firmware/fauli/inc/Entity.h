c0401122996a6a9db080eee9ed35694a694ae479
Nehmulos <nehmu@lavabit.com>
2012-11-18 15:36:51 +0100

    implemented CollisionDetection into Entity

--- firmware/fauli/inc/Entity.h
+++ firmware/fauli/inc/Entity.h
@@ -29,6 +29,9 @@ typedef struct
 	void (*draw)(void*, Bitmap*);
 	void (*destroy)(void*);
 	bool destroyed;
+	
+    bool checkCollisionByPixel;
+	Bitmap* bitmap;
 } Entity;
 
 Entity* Entity_Create(void* context);
@@ -36,5 +39,32 @@ Entity* Entity_Create(void* context);
 void Entity_Update(Entity* entity);
 void Entity_Draw(Entity* entity, Bitmap* surface);
 void Entity_Destroy(Entity* entity);
+
+bool Entity_CheckCollision(Entity* a, Entity* b);
+
+// private
+
+/// stores an absolute x,y and relative width, height
+typedef struct {
+    int x, y, width, height;
+} Rect;
+
+/// using Entity->width Entity->height as rectSize
+/// @return true if the rect of @a is neither left, right above or below the rect of @b
+bool Entity_checkRectCollision(Entity* a, Entity* b);
+/// using Entity->bitmap->width Entity->bitmap->height as rectSize
+/// @return true if the rect of @a is neither left, right above or below the rect of @b
+bool Entity_checkRectCollisionByBitmap(Entity* a, Entity* b);
+/// @return a rect that contains the overlapping area between two sprites
+Rect Entity_collisionSubrect(Entity* a, Entity* b);
+
+////////////////////////////////////////////////////////////////////////////////
+/// Step 1 check for boundingbox collision 
+/// Step 2 find the colliding sub-rectangle
+/// Step 3 check all pixels in the sub-rectangle
+///        and return true if both bitmaps have a pixel that is not transparent
+///        at the same position
+////////////////////////////////////////////////////////////////////////////////
+bool Entity_checkPixelCollision(Entity* a, Entity* b);
 
 #endif
--- firmware/fauli/src/Entity.c
+++ firmware/fauli/src/Entity.c
@@ -22,7 +22,10 @@ Entity* Entity_Create(void* context)
 	entity->update = NULL;
 	entity->draw = NULL;
 	entity->destroy = NULL;
+	
 	entity->destroyed = false;
+	entity->checkCollisionByPixel = false;
+	entity->bitmap = NULL;
 		
 	return entity;
 }
@@ -50,3 +53,59 @@ void Entity_Destroy(Entity* entity)
     }
 	free(entity);
 }
+
+
+
+
+/*
+ Collision
+*/
+
+bool Entity_CheckCollision(Entity* a, Entity* b) {
+    if ((a->checkCollisionByPixel || b->checkCollisionByPixel) && a->bitmap && b->bitmap) {
+        return Entity_checkPixelCollision(a,b);
+    }
+    return Entity_checkRectCollision(a,b);
+}
+
+bool Entity_checkRectCollision(Entity* a, Entity* b) {
+    return !(a->posX < b->posX + b->width) &&
+           !(a->posX + a->width < b->posX) &&
+           !(a->posY < b->posY + b->height) &&
+           !(a->posY + a->height < b->posY);
+}
+
+bool Entity_checkRectCollisionByBitmap(Entity* a, Entity* b) {
+    return !(a->posX < b->posX + b->bitmap->width) &&
+           !(a->posX + a->bitmap->width < b->posX) &&
+           !(a->posY < b->posY + b->bitmap->height) &&
+           !(a->posY + a->bitmap->height < b->posY);
+}
+
+Rect Entity_collisionSubrect(Entity* a, Entity* b) {
+    Rect subrect;
+    subrect.x = a->posX > b->posX ? a->posX : b->posX;
+    subrect.y = a->posY > b->posY ? a->posY : b->posY;
+    subrect.width  = (a->posX + a->bitmap->width  < b->posX + b->bitmap->width  ? a->posX + a->bitmap->width  : b->posX + b->bitmap->width)  - subrect.x;
+    subrect.height = (a->posY + a->bitmap->height < b->posY + b->bitmap->height ? a->posY + a->bitmap->height : b->posY + b->bitmap->height) - subrect.y;
+
+    return subrect;
+}
+
+bool Entity_checkPixelCollision(Entity* a, Entity* b) {
+    if (Entity_checkRectCollisionByBitmap(a,b)) {
+        const Rect collisionRect = Entity_collisionSubrect(a, b);
+    
+        // x and y as absolute global pixel position        
+        for (int y=collisionRect.y; y < collisionRect.y + collisionRect.height; ++y) {
+            for (int x=collisionRect.x; x < collisionRect.x + collisionRect.width; ++x) {
+                if (*ConstBitmapPixelPointer(a->bitmap, x - a->posX, y - a->posY) != TransparentPixel &&
+                    *ConstBitmapPixelPointer(b->bitmap, x - b->posX, y - b->posY) != TransparentPixel) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
