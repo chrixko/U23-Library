3d3fecf24ee9ed3cbbefdeeca0334f72d8a8bf4b
ravensinth <visser.p.b@googlemail.com>
2012-11-19 20:13:11 +0100

    Robo idle

--- firmware/fauli/inc/bilder.h
+++ firmware/fauli/inc/bilder.h
@@ -5,7 +5,7 @@
 
 extern const RLEBitmap * const Sprite_LaserCat[6];
 extern const RLEBitmap bg_lab;
-extern const RLEBitmap * const Sprite_Robo[6];
+extern const RLEBitmap * const Sprite_Robo[12];
 extern const RLEBitmap * const Font_White16[256];
 extern const RLEBitmap Storyboard_back;
 
