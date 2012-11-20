9f8706864812d6e66efee02aa7023bbcd53ba119
ravensinth <visser.p.b@googlemail.com>
2012-11-20 22:41:22 +0100

    Images splitted in different Files

--- firmware/fauli/src/Storyboard.c
+++ firmware/fauli/src/Storyboard.c
@@ -1,14 +1,13 @@
 #include <Storyboard.h>
 
 #include <stdbool.h>
-
 #include <game/Game.h>
 #include <Constants.h>
 #include <Animation.h>
-#include <bilder.h>
 #include <Entity.h>
 #include <game/Font.h>
 #include <Fonts.h>
+#include "Storyboard_Back.h"
 
 
 Storyboard* Storyboard_Create()
@@ -47,7 +46,7 @@ void Storyboard_Draw(void* storyboard, Bitmap* surface)
 {
 	Storyboard* s = storyboard;
 	extern const RLEBitmap storyboard_back;
-	DrawRLEBitmap(surface, &storyboard_back, 0, 0);
+	DrawRLEBitmap(surface, &Storyboard_Back, 0, 0);
 	//setFont(Font_White16x16);
 	setFont(Font_White7x7);
 	DrawText(surface, s-> text, 0,20);
